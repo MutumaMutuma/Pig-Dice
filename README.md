d0ccdafb0d3092e0acf34891d1385863399fe5c0
MutumaMutuma <lewismutuma1000@gmail.com>
2018-07-21 23:35:34 +0300

    worked on my working

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Pig-Dice
+####  2018
+#### By Lewis Mutuma
+## Description
+The Pig Dice is a simple game where two players have a chance to roll a dice.Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to "hold". First player to reach the set winning score wins.
+
+## Pig Dice Rules ##
++ Two  players will compete. The first player to reach the set win score or more is  the winner.
+
++ When it is your turn, you roll. If you get a one, the game is passed to the other player.
++ If you get anything else, you may decide to roll again or pass the pigs.
+
++ If you keep rolling again, you accumulate points to add to your session score but if you get a one, then all your points accumulated in that turn are nullified.
+## Setup/Installation Requirements
+* First of all you need to get access to Github get to my project on Github here is a link <a href="https://github.com/MutumaMutuma/Pig-Dice">Click to Github</a>
+* Clone the project from there.
+* After cloning the project you can view or using it.
+<strong>NOTE:</strong> This project can only open using Google Chrome
+## Known Bugs
+This project cannot be opened in Internet Explorer
+## Technologies Used
+-HTML
+-CSS
+-Bootstrap
+-jQuery libraries
+-JavaScript
+## Support and contact details
+Here is a link to my pig dice game <a href="https://mutumamutuma.github.io/Pig-Dice/"> Click Here to my Pig Dice game</a>
+### License
+MIT License
+Copyright (c) 2018 Lewis Mutuma
--- js/gamescripts.js
+++ js/gamescripts.js
@@ -1,170 +1,91 @@
-//bussiness interface
-var scores, roundScore, activePlayer, gamePlaying;
-var btnRoll = document.querySelector(".btn-roll");
-var btnHold = document.querySelector(".btn-hold");
+var scores, turnScore, activePlayer, diceRoll, gameOn;
 
-//dice images
-var diceimgs = {
-  diceimg01: "https://cdn.pbrd.co/images/70YJMCVVR.png",
-  diceimg02: "https://cdn.pbrd.co/images/711lemsMX.png",
-  diceimg03: "https://cdn.pbrd.co/images/711NjfjV5.png",
-  diceimg04: "https://cdn.pbrd.co/images/712dK3C2z.png",
-  diceimg05: "https://cdn.pbrd.co/images/70Zqc4icX.png",
-  diceimg06: "https://cdn.pbrd.co/images/712DzRw22.png",
-  diceimg11: "https://cdn.pbrd.co/images/713n3lHQN.png",
-  diceimg12: "https://cdn.pbrd.co/images/713JSMJDr.png",
-  diceimg13: "https://cdn.pbrd.co/images/HvoZO4Gb.png",
-  diceimg14: "https://cdn.pbrd.co/images/HvqN3Kjq.png",
-  diceimg15: "https://cdn.pbrd.co/images/714IXBStH.png",
-  diceimg16: "https://cdn.pbrd.co/images/714ZovsdD.png"
-};
-init();
 
-document.querySelector(".btn-roll").addEventListener("click", function() {
-  if (gamePlaying) {
-    // my random numbers
-    var dice = Math.floor(Math.random() * 6) + 1;
+initializeGame();
 
-    // function that will desplay my results
-    var diceDOM = document.querySelector(".dice");
-    diceDOM.style.display = "block";
-    diceDOM.src = diceimgs["diceimg" + activePlayer + dice];
-
-    document.querySelector("#current-" + activePlayer).innerHTML =
-      "<em>" + dice + "</em";
-
-    // 3. Update round score if the rolled number is not 1
-    if (dice !== 1) {
-      hideRolledMsg();
-      //add score
-      roundScore += dice;
-      document.querySelector(
-        "#current-" + activePlayer
-      ).textContent = roundScore;
-    } else {
-      // my disabled button
-
-      disableBtn(btnRoll, 1000);
-      hideRolledMsg();
-      document.querySelector(
-        ".player-" + activePlayer + "-rolled-1"
-      ).style.visibility =
-        "visible";
-      nextPlayer();
+$(document).ready(function() {
+  $(".roll-dice").click(function() {
+    if (gameOn) {
+      diceRoll = Math.floor(Math.random() * 6) + 1;
+      var diceShow = $(".dice");
+      diceShow.attr("src", 'images/throw-' + diceRoll + '.png')
+      diceShow.show();
+      $("#current-" + activePlayer).text(diceRoll);
+      if (diceRoll !== 1) {
+        turnScore += diceRoll;
+        $("#roundscore-" + activePlayer).text(turnScore);
+      } else {
+        nextGamer();
+      }
     }
-  }
-});
 
-document.querySelector(".btn-hold").addEventListener("click", function() {
-  if (gamePlaying) {
-    disableBtn(btnRoll, 1000);
-    // Add current score to global score
-    scores[activePlayer] += roundScore;
+  })
 
-    //Update the UI
-    document.querySelector("#score-" + activePlayer).textContent =
-      scores[activePlayer];
+  $(".hold-score").click(function() {
+    if (gameOn) {
+      scores[activePlayer] += turnScore;
+      $("#score-" + activePlayer).text(scores[activePlayer]);
+      $("#roundscore-0").text(0);
+      $("#roundscore-1").text(0);
+      var finalScore = parseInt($(".final-score").val());
+      var winningScore;
+      if (finalScore) {
+        winningScore = finalScore;
+      } else {
+        winningScore = 50;
+      }
 
-    //how to check if one of my players have won
-    if (scores[activePlayer] >= 50) {
-      document.querySelector("#name-" + activePlayer).textContent = "Winner!";
-      document.querySelector(".dice").style.display = "none";
-      document
-        .querySelector(".player-" + activePlayer + "-panel")
-        .classList.add("winner-" + activePlayer);
-      document
-        .querySelector(".player-" + activePlayer + "-panel")
-        .classList.remove("active-" + activePlayer);
-      gamePlaying = false;
-    } else {
-      nextPlayer();
+      if (scores[activePlayer] >= winningScore) {
+        $("#name-" + activePlayer).text("CONGRATULATIONS!!!");
+        $(".dice").hide();
+        $(".gamer-" + activePlayer + "-section").addClass("winner");
+        $(".gamer-" + activePlayer + "-section").removeClass("playing");
+        $("#roundscore-0").text(0);
+        $("#roundscore-1").text(0);
+        $(".final-score").val("")
+        gameOn = false;
+      } else {
+        nextGamer();
+      }
     }
-  }
-});
+  })
+  $(".new-game").click(function() {
+    initializeGame();
+  })
 
-document.querySelector(".btn-new").addEventListener("click", init);
+})
 
-document.querySelector(".btn-rules").addEventListener("click", function() {
-  var games = document.getElementsByClassName("game-panel");
-  for (i = 0; i < games.length; i++) {
-    games[i].style.display = "none";
-  }
+var nextGamer = function() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  turnScore = 0;
+  $("#current-0").text(0);
+  $("#current-1").text(0);
+  $(".gamer-0-section").toggleClass("playing");
+  $(".gamer-1-section").toggleClass("playing");
+  $(".dice").hide();
+  $("#roundscore-0").text(0);
+  $("#roundscore-1").text(0);
+}
 
-  document.querySelector(".btn-back").style.display = "block";
-  var rules = document.getElementsByClassName("rules-panel");
-  for (i = 0; i < rules.length; i++) {
-    rules[i].style.display = "block";
-  }
-});
-
-document.querySelector(".btn-back").addEventListener("click", function() {
-  var games = document.getElementsByClassName("game-panel");
-  for (i = 0; i < games.length; i++) {
-    games[i].style.display = "block";
-  }
-
-  document.querySelector(".btn-back").style.display = "none";
-  var rules = document.getElementsByClassName("rules-panel");
-  for (i = 0; i < rules.length; i++) {
-    rules[i].style.display = "none";
-  }
-});
-
-
-// Logic interface
-function init() {
+function initializeGame() {
   scores = [0, 0];
-  roundScore = 0;
+  turnScore = 0;
   activePlayer = 0;
-  gamePlaying = true;
-  document.querySelector(".dice").style.display = "none";
-  document.getElementById("score-0").textContent = "0";
-  document.getElementById("score-1").textContent = "0";
-  document.getElementById("current-0").textContent = "0";
-  document.getElementById("current-1").textContent = "0";
-  document.querySelector(".player-0-rolled-1").style.visibility = "hidden";
-  document.querySelector(".player-1-rolled-1").style.visibility = "hidden";
+  gameOn = true;
+  $("#score-0").text(0);
+  $("#score-1").text(0);
+  $("#current-0").text(0);
+  $("#current-1").text(0);
+  $("#roundscore-0").text(0);
+  $("#roundscore-1").text(0);
+  $(".final-score").val("")
+  $("#name-0").text("Player 1");
+  $("#name-1").text("Player 2");
+  $(".gamer-0-section").removeClass("winner");
+  $(".gamer-1-section").removeClass("winner");
+  $(".gamer-0-section").removeClass("playing");
+  $(".gamer-1-section").removeClass("playing");
+  $(".gamer-0-section").addClass("playing");
 
-  document.querySelector("#name-0").textContent = "Player 1";
-  document.querySelector("#name-1").textContent = "Player 2";
-  document.querySelector(".player-0-panel").classList.add("active-0");
-  document.querySelector(".player-0-panel").classList.remove("winner-0");
-  document.querySelector(".player-1-panel").classList.remove("winner-1");
-}
 
-function nextPlayer() {
-  //next player
-  var icons = document.getElementsByTagName("i");
-  for (i = 0; i < icons.length; i++) {
-    icons[i].classList.remove("color-" + activePlayer);
-  }
-
-  document.querySelector(".dice").style.display = "none";
-  document
-    .querySelector(".player-" + activePlayer + "-panel")
-    .classList.remove("active-" + activePlayer);
-  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-  roundScore = 0;
-
-  for (i = 0; i < icons.length; i++) {
-    icons[i].classList.add("color-" + activePlayer);
-  }
-  document
-    .querySelector(".player-" + activePlayer + "-panel")
-    .classList.add("active-" + activePlayer);
-  document.querySelector("#current-0").textContent = "0";
-  document.querySelector("#current-1").textContent = "0";
-}
-
-function disableBtn(btn, time) {
-  //disable button
-  btn.disabled = true;
-  setTimeout(function() {
-    btn.disabled = false;
-  }, time);
-}
-
-function hideRolledMsg() {
-  document.querySelector(".player-0-rolled-1").style.visibility = "hidden";
-  document.querySelector(".player-1-rolled-1").style.visibility = "hidden";
-}
+};
